738565327a4bec5c5838da44d4d81a766c0336bd
Lijith P <lijith.ramachandran@gmail.com>
2020-04-26 12:38:44 +0530

    Implementing the bottom bar and drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:test_project/src/screens/HomePage.dart';
+import 'package:test_project/src/screens/NotificationPage.dart';
+import 'package:test_project/src/screens/UploadPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Let's Cook",
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -44,18 +48,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _selectedPage = 0;
+  final _pageOptions = [HomePage(), UploadPage(), NotificationPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -69,43 +63,58 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Color.fromARGB(255, 255, 59, 48),
+        /*leading: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(50, 255, 255, 255),
+              borderRadius: BorderRadius.all(Radius.circular(22.0))),
+        ),*/
+        title: Stack(children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 12.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(50, 255, 255, 255),
+                borderRadius: BorderRadius.all(Radius.circular(22.0))),
+            /*child: Text('Search Recipes')*/
+          ),
+          IconButton(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            icon: Icon(Icons.search),
+            onPressed: null,
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              alignment: Alignment.center,
+              child: Text("Search Recipes",
+                  style: TextStyle(color: Colors.white38))),
+        ]),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: new Drawer(
+          child: new ListView(
+        children: <Widget>[
+          new ListTile(
+              leading: new Icon(Icons.settings), title: Text('Settings')),
+          new ListTile(leading: new Icon(Icons.toys), title: Text('Version'))
+        ],
+      )),
+      body: _pageOptions[_selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Explore')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.cloud_upload), title: Text('Upload')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), title: Text('Notification'))
+          ]),
     );
   }
 }
